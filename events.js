7114749b31a2c67996afa162688fc2a0b9ab8ec8
SujayBWJ <sujaybharadwaj.dev@gmail.com>
2025-06-23 22:56:50 +0530

    added separately

---
+++ events.js
@@ -0,0 +1,18 @@
+document.getElementById("React").addEventListener("click", function () {
+  window.location.href = "https://forms.gle/ViLPqgKKzGH8eUuk7";
+});
+document.getElementById("Hackathon").addEventListener("click", function () {
+  window.location.href = "https://forms.gle/jNJiSGRY5FEoao6c8";
+});
+document.getElementById("Py").addEventListener("click", function () {
+  window.location.href = "https://forms.gle/HXzNGKMp7z1wbz8c6";
+});
+document.getElementById("Ind").addEventListener("click", function () {
+  window.location.href = "https://forms.gle/Fwoq9UHifyFT8dKQ9";
+});
+document.getElementById("web").addEventListener("click", function () {
+  window.location.href = "https://forms.gle/oMdG1V4iEH7AtupN9";
+});
+document.getElementById("code").addEventListener("click", function () {
+  window.location.href = "https://forms.gle/fopGsMDeSZJDogiU9";
+});
