547114f3b28f2fdd10c8e7b4aaa7794ad11892a2
SujayBWJ <sujaybharadwaj.dev@gmail.com>
2025-06-23 22:09:26 +0530

    created repo

---
+++ website/script.js
@@ -0,0 +1,6 @@
+ document.getElementById("joinBtn").addEventListener("click", function () {
+  window.location.href = "https://forms.gle/aX4bYN1ttkSpCpwj7";
+});
+document.getElementById("login").addEventListener("click", function () {
+  window.location.href = "/login.html";
+});
